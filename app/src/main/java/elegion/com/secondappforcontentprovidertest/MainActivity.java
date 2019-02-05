023cd3e0c9f92bbbd858e8205d1499557844cb98
savelgmx <savel@gmx.com>
2019-02-05 16:36:44 +0700

    №8
            //TODO реализовать в пернесенном методе логику работы согласно ТЗ

--- app/src/main/java/elegion/com/secondappforcontentprovidertest/MainActivity.java
+++ app/src/main/java/elegion/com/secondappforcontentprovidertest/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,27 +18,35 @@ import android.widget.Toast;
 import static java.lang.String.valueOf;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener {
+
     /*
 
-        В проекте SecondAppForContentProviderTestSecondAppForContentProviderTest определить вертикальный LinearLayout.---
+            В проекте SecondAppForContentProviderTestSecondAppForContentProviderTest определить вертикальный LinearLayout.---
 
-        Добавить на разметку 2 спиннера  ----
-       -  в одном “Albums”, “Songs”, "AlbumSongs"
-                -  во втором - “query”, “insert”, “update”, “delete”; Строки захардкожены.
+            Добавить на разметку 2 спиннера  ----
+           -  в одном “Albums”, “Songs”, "AlbumSongs"
+                    -  во втором - “query”, “insert”, “update”, “delete”; Строки захардкожены.
 
-        Добавить EditText, в который в последствии будет вписываться id элемента.
-        Если EditText пустой, то валидным считается только “query” и “insert”, на методы delete() и update() показывать тост с ошибкой.
-        Добавить Button (далее - кнопка действия), при нажатии на который будет выполняться выбранное во втором спиннере
-        действие с выбранной в первом спиннере таблицей и вписанным id в первый EditText.
-        Действие реализовать с помощью соответствующих методов getContentResolver() (query() / insert() / update() / delete(), пример есть в проекте)
-        Если в EditText (id) вписано не число или число, но записи под таким id в таблице нет, то при нажатии выводить тост - “Ошибка id”
-        Добавить три EditText’а (далее - EditText c данными), из которых будут заполнятся данные ContentValues для методов insert() и update()
-        Проверка валидности происходит с помощью метода query(), который должен показывать тост с текущими данными в выбранной таблице.
-    */
+            Добавить EditText, в который в последствии будет вписываться id элемента.
+            Если EditText пустой, то валидным считается только “query” и “insert”, на методы delete() и update() показывать тост с ошибкой.
+            Добавить Button (далее - кнопка действия), при нажатии на который будет выполняться выбранное во втором спиннере
+            действие с выбранной в первом спиннере таблицей и вписанным id в первый EditText.
+            Действие реализовать с помощью соответствующих методов getContentResolver() (query() / insert() / update() / delete(), пример есть в проекте)
+            Если в EditText (id) вписано не число или число, но записи под таким id в таблице нет, то при нажатии выводить тост - “Ошибка id”
+            Добавить три EditText’а (далее - EditText c данными), из которых будут заполнятся данные ContentValues для методов insert() и update()
+            Проверка валидности происходит с помощью метода query(), который должен показывать тост с текущими данными в выбранной таблице.
+        */
     private Spinner tables_sp;//--определяем -Spinner tables_sp
     private Spinner query_sp;//------еще Спиннер с вариантами запроса
     private Button mQueryBtn;
-    private EditText etId;
+    private EditText mEditTextId;
+
+    private String tables_sp_selected;
+    private Uri uri;
+
+    private static final String LOG_TAG ="SecondApp" ;
+
+     final Uri ALBUM_URI = Uri.parse("content://ru.startandroid.providers.AdressBook/contacts");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +54,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
         tables_sp =findViewById(R.id.tables_sp);
         query_sp = findViewById(R.id.query_sp);
-        etId =findViewById(R.id.et_id);
+        mEditTextId =findViewById(R.id.et_id);
 
-
-//TODO Перенести OnClick из метода OnCreate
         //TODO реализовать в пернесенном методе логику работы согласно ТЗ
 
         mQueryBtn = (Button)findViewById(R.id.QueryBtn);
@@ -56,36 +63,86 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         getSupportLoaderManager().initLoader(1, null, this);
 
+
+    }
+
+    private boolean ismEditTextIdEmpty(){
+        return mEditTextId.getText().toString().isEmpty();
+    }
+
+
+    private Uri SelectTableUri(){
+
+        if(this.tables_sp_selected.toLowerCase().equals("albums")){
+            uri =Uri.parse("content://com.elegion.roomdatabase.musicprovider/album/");
+        }
+        if(this.tables_sp_selected.toLowerCase().equals("songs")){
+            uri = Uri.parse("content://com.elegion.roomdatabase.musicprovider/songs/");
+        }
+        if(this.tables_sp_selected.toLowerCase().equals("albumsongs")) {
+            uri = Uri.parse("content://com.elegion.roomdatabase.musicprovider/songs/");
+        }
+        Log.d(LOG_TAG,"Uri ="+uri);
+            return uri;
     }
 
     @Override
     public void onClick(View v) {
 
-        String tables_sp_selected= tables_sp.getSelectedItem().toString();
+        tables_sp_selected= tables_sp.getSelectedItem().toString();
         Toast.makeText(getApplicationContext(), tables_sp_selected, Toast.LENGTH_SHORT).show();
         String query_sp_selected= query_sp.getSelectedItem().toString();
         Toast.makeText(getApplicationContext(), query_sp_selected, Toast.LENGTH_SHORT).show();
+
+
+        uri =SelectTableUri();
+
+
         //выбираем действие в зависимости от типа запроса
         if(query_sp_selected.toLowerCase().equals("query")) {
             Toast.makeText(getApplicationContext(), "selected query", Toast.LENGTH_SHORT).show();
+            try{
+
+            }catch (Exception ex){
+
+            }
+
+            getContentResolver().query(uri,
+                    null,
+                    null,
+                    null,
+                    null);
+
+            Uri uri = Uri.parse("content://ru.startandroid.providers.AdressBook/phones");
+            try {
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            } catch (Exception ex) {
+                Log.d(LOG_TAG, "Error: " + ex.getClass() + ", " + ex.getMessage());
+            }
         }
         if(query_sp_selected.toLowerCase().equals("insert")) {
             Toast.makeText(getApplicationContext(), "selected insert", Toast.LENGTH_SHORT).show();
         }
 
-        if(query_sp_selected.toLowerCase().equals("update")){
+
+        if(query_sp_selected.toLowerCase().equals("update") && !ismEditTextIdEmpty() ){
             Toast.makeText(getApplicationContext(),"selected update",Toast.LENGTH_SHORT).show();
             ContentValues contentValues = new ContentValues();
             contentValues.put("id", 0);
             contentValues.put("name", "new Name");
             contentValues.put("release", "tomorrow");
             getContentResolver().update(Uri.parse("content://com.elegion.roomdatabase.musicprovider/album/1"), contentValues, null, null);
-
-
+        }
+        else if(query_sp_selected.toLowerCase().equals("update") && ismEditTextIdEmpty() ){
+            Toast.makeText(getApplicationContext(),"Ошибка Операции Update. Id не должен быть пустым" ,Toast.LENGTH_SHORT).show();
         }
 
-        if(query_sp_selected.toLowerCase().equals("delete")){
+        if(query_sp_selected.toLowerCase().equals("delete") && !ismEditTextIdEmpty()){
             Toast.makeText(getApplicationContext(),"selected delete",Toast.LENGTH_SHORT).show();
+        }
+        else if(query_sp_selected.toLowerCase().equals("delete") && ismEditTextIdEmpty()){
+            Toast.makeText(getApplicationContext(),"Ошибка Операции Delete. Id не должен быть пустым" ,Toast.LENGTH_SHORT).show();
+
         }
 
 
